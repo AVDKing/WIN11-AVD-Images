b6e0b57c8c8f82eaa6de558c90d1981235f50d50
AVDKing <98476411+AVDKing@users.noreply.github.com>
2022-02-24 13:11:19 +0100

    Update Domainjoin.ps1

--- Domainjoin.ps1
+++ Domainjoin.ps1
@@ -25,15 +25,4 @@
     $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $user, $pass
     Add-Computer -DomainName sepagoavd.onmicrosoft.com -Credential $cred  -Restart
 
-    #FS-Logix Gruppen Anpassung Teil 2
-    Get-LocalGroup -Name "FSLogix ODFC Include List"
-    Add-LocalGroupMember -Name "FSLogix ODFC Include List" -Member $FSLogixUsers
- 
-    Get-LocalGroup -Name "FSLogix Profile Include List"
-    Add-LocalGroupMember -Name "FSLogix ODFC Include List" -Member $FSLogixUsers
     
-    Get-LocalGroup -Name "FSLogix Profile Exclude List"
-    Add-LocalGroupMember -Name "FSLogix Profile Exclude List" -Member $ExcludeUsers
-
-    Get-LocalGroup -Name "FSLogix ODFC Exclude List"
-    Add-LocalGroupMember -Name "FSLogix Profile Exclude List" -Member $ExcludeUsers

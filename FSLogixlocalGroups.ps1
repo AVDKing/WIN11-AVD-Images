57e7e5ccb96ed61e18189589501bf5cff816337d
AVDKing <98476411+AVDKing@users.noreply.github.com>
2022-03-08 14:26:48 +0100

    Update FSLogixlocalGroups.ps1

--- FSLogixlocalGroups.ps1
+++ FSLogixlocalGroups.ps1
@@ -1,8 +1,12 @@
 #Variablen
  $user = "LocAdmin"
- $pass=ConvertTo-SecureString "123AVDherbei!" -AsPlainText -Force
+ $pass=ConvertTo-SecureString "123AVDherbei" -AsPlainText -Force
  $FSLogixUsers = "sepagoavd.onmicrosoft.com\AVDUsers"
  $ExcludeUsers = "sepagoavd.onmicrosoft.com\Domain Admins"
+
+$secpassword = ConvertTo-SecureString "123AVDherbei!" -AsPlainText –Force
+ $mycreds = New-Object System.Management.Automation.PSCredential ("LocAdmin", $secpassword)
+Start-Process powershell.exe -Credential Get-Credential -ArgumentList test.ps1
 
 
 #FS-Logix Gruppen Anpassung Teil 2
